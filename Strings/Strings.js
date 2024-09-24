6ced1663739bafef10445e364f65726fdb616eea
DavidAlonso858 <davidmadrid858@gmail.com>
2024-09-24 14:43:01 +0200

    let str = `hi${person}, are you in ${city}`;

---
+++ Strings/Strings.js
@@ -0,0 +1,9 @@
+let person = "nuria";
+let city = "malaga";
+let str = `hi${person}, are you in ${city}`;
+console.log(str);
+
+let str2 = `hi ${person}, 
+are you in:
+ ${city}`;
+console.log(str2);

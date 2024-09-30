1d82a367d4e62c518813d13206976882c1aec0d2
david <dalogar0801@g.educaand.es>
2024-09-30 18:06:14 +0200

    Arrays mapas const numeros = [1, 2, 3, 4];
    function traditional(numeros) {
        let numeroArray = []
        for (const e of numeros) {
            numeroArray.push(Math.pow(e, 2));
        }
        return numeroArray;
    }
    console.log(traditional(numeros));
    
    const numerosCuadrados = numeros.map(e => {
        let numero = Math.pow(e, 2);
        return numero;
    });
    
    console.log(numerosCuadrados);

---
+++ EjerciciosArrays/Ejercicio1map.js
@@ -0,0 +1,7 @@
+const people = ["Andres", "David", "Ivan", "Jesus"];
+
+const peopleGreeting = people.map (e => {
+    let nombre = e;
+    console.log("Have a good day", nombre);
+});
+
---
+++ EjerciciosArrays/Ejercicio2.js
@@ -0,0 +1,19 @@
+const numeros = [1, 2, 3, 4];
+
+
+function traditional(numeros) {
+    let numeroArray = []
+    for (const e of numeros) {
+        numeroArray.push(Math.pow(e, 2));
+    }
+    return numeroArray;
+}
+console.log(traditional(numeros));
+
+
+const numerosCuadrados = numeros.map(e => {
+    let numero = Math.pow(e, 2);
+    return numero;
+});
+
+console.log(numerosCuadrados);

6947504dd3e5baeca0c9fe7c173c7b6efa4717e1
david <dalogar0801@g.educaand.es>
2024-10-02 22:20:49 +0200

    let letras = params.split(" ");

---
+++ EjerciciosStrings/Ejercicio5split.js
@@ -0,0 +1,12 @@
+function separar(params) {
+    let letras = params.split(" "); // para separarlo por el espacio
+
+    let primero = letras[0];
+    let segundo = letras[1] ? letras[1][0] + "." : ""; // si existe apellido pillar primera letra
+
+    return `${primero} ${segundo}`;
+}
+
+console.log(separar("Robin Singh"));
+
+

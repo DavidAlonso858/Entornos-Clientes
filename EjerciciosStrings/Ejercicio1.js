39c606b7df43647ea8e430f72b9b6aef3e89dd3b
david <dalogar0801@g.educaand.es>
2024-10-02 20:30:37 +0200

    Ejercicio string console.log(`\`${apellido}\``);

---
+++ EjerciciosStrings/Ejercicio1.js
@@ -0,0 +1,20 @@
+let nombre = "David";
+let apellido = 'Alonso';
+
+console.log('"',nombre,'"');
+console.log("'",nombre,"'");
+console.log("`",nombre,"`");
+
+console.log(`\n"${nombre}"`);
+console.log(`'${nombre}'`);
+console.log(`\`${nombre}\``);
+
+
+console.log('\n"',apellido,'"');
+console.log("'",apellido,"'");
+console.log("`",apellido,"`");
+
+console.log(`\n"${apellido}"`);
+console.log(`'${apellido}'`);
+console.log(`\`${apellido}\``);
+

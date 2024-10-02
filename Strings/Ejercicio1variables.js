1340a05c69a74ecc09c883d046906004c637b80e
DavidAlonso858 <davidmadrid858@gmail.com>
2024-10-02 11:13:54 +0200

    Strings ejercicios

---
+++ Strings/Ejercicio1variables.js
@@ -0,0 +1,7 @@
+let nombre = 'David';
+let apellido = "Alonso";
+let apellido2 = `Garcia`;
+
+
+console.log("'", nombre, "'", '"', apellido, '"', "`", apellido2, "`");
+console.log(`'${nombre}',"${apellido}",\`${apellido2}\``);

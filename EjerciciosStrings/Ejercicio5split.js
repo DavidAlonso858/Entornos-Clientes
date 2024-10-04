6dcbb3469ba2983045ad4c24ab56ea0e53ce0464
DavidAlonso858 <davidmadrid858@gmail.com>
2024-10-04 10:13:32 +0200

    hecho con foreach letras.forEach((palabra, index) => {         if (index === 0) {             // La primera palabra es el nombre completo             reformado += palabra;         } else if (index === 1) {             // La segunda palabra es el apellido, tomamos la primera letra y la agregamos con un punto             reformado += " " + palabra[0] + ".";         }     });

--- EjerciciosStrings/Ejercicio5split.js
+++ EjerciciosStrings/Ejercicio5split.js
@@ -1,12 +1,23 @@
 function separar(params) {
     let letras = params.split(" "); // para separarlo por el espacio
-
+    let reformado = "";
     let primero = letras[0];
     let segundo = letras[1] ? letras[1][0] + "." : ""; // si existe apellido pillar primera letra
 
     // Hacerlo con forEach
+    letras.forEach((palabra, index) => {
+        if (index === 0) {
+            // La primera palabra es el nombre completo
+            reformado += palabra;
+        } else if (index === 1) {
+            // La segunda palabra es el apellido, tomamos la primera letra y la agregamos con un punto
+            reformado += " " + palabra[0] + ".";
+        }
+    });
 
-    return `${primero} ${segundo}`;
+    return reformado;
+
+   // return `${primero} ${segundo}`;
 }
 
 console.log(separar("Robin Singh"));

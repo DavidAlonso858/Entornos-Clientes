aab734701191149224fa12d05d7ec5f004504de6
david <dalogar0801@g.educaand.es>
2024-10-06 13:02:11 +0200

    JSON libros creados

---
+++ ActividadJSON/librosJSON.js
@@ -0,0 +1,47 @@
+let arrayLibros = [
+    {
+        Title: "Paradise Lost",
+        Genre: "Poem",
+        Author: "John Milton",
+        Pages: 453,
+        Date: "10/05/1667",
+        Read: false,
+        Website: ""
+    },
+    {
+        Title: "Don Quijote de la Mancha",
+        Genre: "Parody",
+        Author: "Miguel de Cervantes",
+        Pages: 1560,
+        Date: "16/01/1605",
+        Read: true,
+        Website: ""
+    },
+    {
+        Title: "Dune",
+        Genre: "Epic Science Fiction ",
+        Author: "Frank Herbert",
+        Pages: 784,
+        Date: "01/08/1965",
+        Read: true,
+        Website: ""
+    },
+    {
+        Title: "Berserk",
+        Genre: "Dark Fantasy",
+        Author: "Kentaro Miura",
+        Pages: 8000,
+        Date: "26/11/1990",
+        Read: true,
+        Website: ""
+    },
+    {
+        Title: "The Stranger",
+        Genre: "Existentialism",
+        Author: "Albert Camus",
+        Pages: 159,
+        Date: "19/05/1942",
+        Read: false,
+        Website: ""
+    },
+]

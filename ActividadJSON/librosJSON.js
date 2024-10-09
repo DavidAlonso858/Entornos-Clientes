5f20d2f2c95499534042e7f5efce126f72309d62
david <dalogar0801@g.educaand.es>
2024-10-09 20:50:10 +0200

    Hacer elementos HTML en JS y botones document.querySelector(".lista")

--- ActividadJSON/librosJSON.js
+++ ActividadJSON/librosJSON.js
@@ -48,3 +48,88 @@ let arrayLibros = [
 arrayLibros.forEach(libro => {
     console.log(libro);
 });
+
+
+document.querySelector(".inicio").innerHTML = "Showcase Books";
+
+// HACER TABLA DESDE JS createElement
+function mostrario() {
+    const tabla = document.createElement("table");
+
+    const apartados = document.createElement("tr");
+    const encabezado = ["Title", "Genre", "Author", "Pages", "Date", "Read", "Website"];
+
+    encabezado.forEach(e => {
+        const primera = document.createElement("th");
+        primera.textContent = e;
+
+        apartados.appendChild(primera);
+    });
+
+    tabla.appendChild(apartados);
+
+    arrayLibros.forEach(libro => {
+        const fila = document.createElement("tr");
+
+        for (const key in libro) {
+            const columna = document.createElement("td");
+            columna.textContent = libro[key];
+
+            fila.appendChild(columna);
+        }
+
+        tabla.appendChild(fila);
+    });
+
+    return tabla;
+}
+
+document.querySelector(".tablaLibros").append(mostrario())
+
+// FUNCIONES DE CADA BOTON
+function nombraGeneros(arrayLibros) {
+    const arrayGeneros = [];
+
+    arrayLibros.forEach(l => {
+        arrayGeneros.push(l.Genre);
+    })
+    return arrayGeneros.join("<br>")
+}
+
+document.querySelector(".boton1").addEventListener("click", function () {
+    document.querySelector(".contenidoGeneros").innerHTML = nombraGeneros(arrayLibros)
+});
+
+function librosLargos(arrayLibros) {
+    const arrayLibrosLargos = arrayLibros.filter(l => l.Pages > 300);
+
+    arrayLibros = arrayLibrosLargos.map(l => l.Title);
+
+    return arrayLibros.join("<br>")
+}
+
+document.querySelector(".boton2").addEventListener("click", function () {
+    document.querySelector(".libros300").innerHTML = librosLargos(arrayLibros)
+});
+
+
+// function masDosAños(arrayLibros) {
+//     const fechaActual = new Date();
+
+//     const arrayDosAños = arrayLibros.filter(l => {
+//         const fecha = new Date(l.Date)
+//         const diferenciaEnMilisegundos = fechaActual - fecha; // Calcular la diferencia en milisegundos
+
+//         // Comparar si la diferencia es mayor a dos años (en milisegundos)
+//         return diferenciaEnMilisegundos > 2 * 365 * 24 * 60 * 60 * 1000;
+//     });
+
+//     arrayDosAños = arrayDosAños.map(l => l.Title);
+
+//     return arrayDosAños.join("<br>");
+// }
+
+// document.querySelector(".boton3").addEventListener("click", function () {
+//     document.querySelector(".dosAños").innerHTML = masDosAños(arrayLibros);
+// });
+

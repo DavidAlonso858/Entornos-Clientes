76df694071118882b19e8c7316c3dbf98cdd21c6
david <dalogar0801@g.educaand.es>
2024-11-24 15:03:09 +0100

    estructura proyecto

---
+++ Proyecto-Directores-Peliculas/README.md
@@ -0,0 +1,5 @@
+# Proyecto de filmografia Director-Pelicula
+
+Este proyecto es una aplicacion web que sirve para gestionar a diferentes directores y sus peliculas a través de un CRUD (Crear, Leer, Actualizar, Eliminar). En mi caso lo voy a centrar en el genero de terror.
+
+# Al usar Node.js lo ejecuto con npx live-server
---
+++ Proyecto-Directores-Peliculas/assests/directores.js
@@ -0,0 +1,32 @@
+let almacenDirectores = [
+    {
+        id: 1,
+        nombre: "David Cronenberg",
+        edad: 81
+    },
+    {
+        id: 2,
+        nombre: "Ti West",
+        edad: 44
+    },
+    {
+        id: 3,
+        nombre: "Alfred Hitchcock",
+        edad: 80
+    },
+    {
+        id: 4,
+        nombre: "Paco Plaza",
+        edad: 51
+    },
+    {
+        id: 5,
+        nombre: "Guillermo del Toro",
+        edad: 60
+    },
+    {
+        id: 6,
+        nombre: "James Wan",
+        edad: 47
+    }
+]

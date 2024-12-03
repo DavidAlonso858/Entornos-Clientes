3375cfa497d6151b75d469c68a21815a1906ecec
david <dalogar0801@g.educaand.es>
2024-12-03 20:17:42 +0100

    tarjeta mejor

--- Tema JavaScript, DOM, JSON, FORM/DOM-Form/tarjeta.js
+++ Tema JavaScript, DOM, JSON, FORM/DOM-Form/tarjeta.js
@@ -1,99 +1,120 @@
-/* 
-convertir a mayuscula cuando pierde el foco(blur)
-*/
+document.querySelector("#boton").addEventListener("click", function () {
+    const formatoCorrecto = true;
 
-function nombre() {
-    const cambioNombre = document.querySelector("#nombre");
-    cambioNombre.value = cambioNombre.value.toUpperCase()
+    const numSecreto = document.querySelector("#numSecreto");
+    const nombre = document.querySelector("#nombre");
+    const numTarjeta = document.querySelector("#numTarjeta");
+    const fecha = document.querySelector("#fecha");
 
-    if (cambioNombre.value.length < 7) {
-        alert("minimo 7 caracteres");
+    if (!nombre.checkValidity()) {
+        nombre.classList.add("is-invalid");
+        formatoCorrecto = false
+    } else {
+        nombre.classList.remove("is-invalid");
+        nombre.classList.add("is-valid");
     }
-    let valor = cambioNombre.value;
+
+
+    if (!numSecreto.checkValidity()) {
+        numSecreto.classList.add("is-invalid");
+        formatoCorrecto = false
+    } else {
+        numSecreto.classList.remove("is-invalid");
+        numSecreto.classList.add("is-valid");
+    }
+
+
+    if (!numTarjeta.checkValidity()) {
+        numTarjeta.classList.add("is-invalid");
+        formatoCorrecto = false
+    } else {
+        numTarjeta.classList.remove("is-invalid");
+        numTarjeta.classList.add("is-valid");
+    }
+
+
+    if (!fecha.checkValidity()) {
+        fecha.classList.add("is-invalid");
+        formatoCorrecto = false
+    } else {
+        fecha.classList.remove("is-invalid");
+        fecha.classList.add("is-valid");
+    }
+})
+
+function cantidad() {
+    const cantidad = document.querySelector("#amount");
+    const contidad2 = cantidad.value;
+
+    let valorbueno = contidad2.replace(",", ".");
+
+    if (!valorbueno.includes("€")) {
+        valorbueno += "€";
+    }
+
+    cantidad.value = valorbueno;
+}
+
+function validarNombre() {
+    const valor = document.querySelector("#nombre");
     let contador = 0;
-    for (let index = 0; index < valor.length; index++) {
-        console.log('p');
-        if (valor.charAt[index] === ' ') {
-            
+    const valor2 = valor.value;
+
+    for (let index = 0; index < valor2.length; index++) {
+        if (valor2.chartAt(index) === " ") {
             contador++;
         }
     }
 
-    if (contador == 0) {
-        alert("Debes poner un apellido al menos");
+    if (contador === 0) {
+        valor.classList.add("is-invalid");
     }
 }
 
-function formatoTarjeta() {
-    const numeroTarjeta = document.querySelector("#numero");
-    console.log(typeof numeroTarjeta.value);
+function validarTarjeta() {
+    const tarjeta = document.querySelector("#numTarjeta");
+    const tarjeta2 = tarjeta.value;
+    let valorBueno = tarjeta2.replace(/[^0-9]/g, "");
 
-    let valorCambiado = numeroTarjeta.value.replace(/\s/g, ""); /* para pillar todos los tipos de espacios*/
-
-    let valorBueno = "";
-    for (let index = 0; index < valorCambiado.length; index++) {
+    let formateo = "";
+    for (let index = 0; index < valorBueno.length; index++) {
         if (index > 0 && index % 4 === 0) {
-            valorBueno += " ";
+            formateo += " ";
         }
-        valorBueno += valorCambiado[index];
+        formateo += valorBueno.charAt(index);
     }
 
-    numeroTarjeta.value = valorBueno;
+    tarjeta.value = formateo;
 }
 
-function cantidad() {
-    const cantidadAlmacenada = document.querySelector("#cantidad");
+function numTarjeta() {
+    const num = document.querySelector("#numSecreto")
+    const num2 = num.value;
 
-    let valor = cantidadAlmacenada.value;
-    let nuevoValor = "";
+    let valorBueno = num2.replace(/[^0-9]/g, "");
 
-    console.log(typeof valor);
+    num.value = valorBueno;
+}
 
-    if (valor.includes(',')) {
-        nuevoValor = valor.replace(',', '.');
+function validarFecha() {
+    const fechaInput = document.querySelector("#fecha");
+    const valor = fechaInput.value;
 
-    } else {
-        nuevoValor = valor
-        nuevoValor += ".00";
+    let valorBueno = valor.replace(/[^0-9]/g, "");
+
+    let formateo = "";
+    for (let index = 0; index < valorBueno.length; index++) {
+        if (index === 2) {
+            formateo += "/";
+        }
+        formateo += valorBueno.charAt(index);
     }
 
-    cantidadAlmacenada.value = nuevoValor;
-}
-function fecha() {
-    const fechaActual = Date.now();
-    const fechaIntro = new Date(document.querySelector("#fecha").value)
-    if (fechaIntro < fechaActual) {
-        alert(`La fecha debe ser anterior a la actual`);
-    }
+    fechaInput.value = formateo;
 }
 
-document.querySelector("#submitButton").addEventListener("click", nombre);
-document.querySelector("#submitButton").addEventListener("click", formatoTarjeta);
-document.querySelector("#submitButton").addEventListener("click", fecha);
-document.querySelector("#submitButton").addEventListener("click", cantidad);
-
-
-/* Destacar focus*/
-document.querySelector("#nombre").addEventListener("focus", function () {
-    let cambio = document.querySelector("#nombre")
-    cambio.style.backgroundColor = "yellow";
-})
-
-document.querySelector("#numero").addEventListener("focus", function () {
-    let cambio = document.querySelector("#numero")
-    cambio.style.backgroundColor = "yellow";
-})
-
-document.querySelector("#fecha").addEventListener("focus", function () {
-    let cambio = document.querySelector("#fecha")
-    cambio.style.backgroundColor = "yellow";
-})
-
-document.querySelector("#seguridad").addEventListener("focus", function () {
-    let cambio = document.querySelector("#seguridad")
-    cambio.style.backgroundColor = "yellow";
-})
-document.querySelector("#cantidad").addEventListener("focus", function () {
-    let cambio = document.querySelector("#cantidad")
-    cambio.style.backgroundColor = "yellow";
-})
+document.querySelector("#nombre").addEventListener("blur", validarNombre);
+document.querySelector("#amount").addEventListener("blur", cantidad);
+document.querySelector("#numTarjeta").addEventListener("input", validarTarjeta);
+document.querySelector("#numSecreto").addEventListener("input", numTarjeta);
+document.querySelector("#fecha").addEventListener("input", validarFecha);

2ac208d5c6345c30aa9d20dec79db4f4e06ae314
DavidAlonso858 <davidmadrid858@gmail.com>
2024-12-10 08:26:14 +0100

    examen con filtro mejor y update nota no tanto

--- EXAMENDA/examenDA.js
+++ EXAMENDA/examenDA.js
@@ -98,165 +98,45 @@ MostrarAnno()
 añadirPaises()
 
 
-function busquedaTitulo() {
-    event.preventDefault();
-
-    const valorTexto = document.querySelector("#texto");
-    const cuerpo = document.querySelector("#cuerpo");
-    const valor2 = valorTexto.value.trim().toLowerCase();
-
-    const busqueda = pelis.filter(p => p.Title.toLowerCase().includes(valor2));
-    // numero busqueda
-    const parrafo = document.createElement("p")
-    parrafo.className = "m-5 fw-bold"
-    parrafo.innerHTML = busqueda.length;
-    cuerpo.appendChild(parrafo);
-
-    if (busqueda) {
-        busqueda.forEach(b => {
-            const div = document.createElement("div");
-            const img = document.createElement("img");
-            const h3 = document.createElement("h3");
-            const generos = document.createElement("p");
-            const boton = document.createElement("button");
-
-            h3.innerHTML = b.Title;
-
-
-            img.src = b.Images[0];
-            img.width = 200
-            img.height = 200
-
-            generos.innerHTML = b.Genre;
-
-            boton.innerHTML = "Details"
-            boton.className = "btn bg-warning text-dark"
-
-
-            div.appendChild(img);
-            div.appendChild(h3);
-            div.appendChild(generos);
-            div.appendChild(boton)
-
-            cuerpo.appendChild(div)
-        })
-
-    }
-
-}
-
-function busquedaDirector() {
-    event.preventDefault();
-
-    const valorTexto = document.querySelector("#texto");
-    const cuerpo = document.querySelector("#cuerpo");
-    const valor2 = valorTexto.value.trim().toLowerCase();
-
-
-
-    const busqueda = pelis.filter(p => p.Director.toLowerCase().includes(valor2));
-    // numero busqueda
-    const parrafo = document.createElement("p")
-    parrafo.className = "m-5 fw-bold"
-    parrafo.innerHTML = busqueda.length;
-    cuerpo.appendChild(parrafo);
-    if (busqueda) {
-        busqueda.forEach(b => {
-            const div = document.createElement("div");
-            const img = document.createElement("img");
-            const h3 = document.createElement("h3");
-            const generos = document.createElement("p");
-            const boton = document.createElement("button");
-
-
-            h3.innerHTML = b.Title;
-
-
-            img.src = b.Images[0];
-            img.width = 200
-            img.height = 200
-
-            generos.innerHTML = b.Genre;
-
-            boton.innerHTML = "Details"
-            boton.className = "btn bg-warning text-dark"
-
-            div.appendChild(img);
-            div.appendChild(h3);
-            div.appendChild(generos);
-            div.appendChild(boton)
-
-
-            cuerpo.appendChild(div)
-        })
-
-    }
-
-}
-
-function busquedaActores() {
-    event.preventDefault();
-
-    const valorTexto = document.querySelector("#texto");
-    const cuerpo = document.querySelector("#cuerpo");
-    const valor2 = valorTexto.value.trim().toLowerCase();
-
-
-    const busqueda = pelis.filter(p => p.Actors.toLowerCase().includes(valor2));
-    // numero busqueda
-    const parrafo = document.createElement("p")
-    parrafo.className = "m-5 fw-bold"
-    parrafo.innerHTML = busqueda.length;
-    cuerpo.appendChild(parrafo);
-    if (busqueda) {
-        busqueda.forEach(b => {
-            const div = document.createElement("div");
-            const img = document.createElement("img");
-            const h3 = document.createElement("h3");
-            const generos = document.createElement("p");
-            const boton = document.createElement("button");
-
-            h3.innerHTML = b.Title;
-
-
-            img.src = b.Images[0];
-            img.width = 200
-            img.height = 200
-
-            generos.innerHTML = b.Genre;
-
-            boton.innerHTML = "Details"
-            boton.className = "btn bg-warning text-dark"
-
-            div.appendChild(img);
-            div.appendChild(h3);
-            div.appendChild(generos);
-            div.appendChild(boton)
-
-            cuerpo.appendChild(div)
-        })
-
-    }
-
-}
-
 function busquedaGeneral() {
     event.preventDefault();
-    const valor = document.querySelector("#texto");
+    const valorTexto = document.querySelector("#texto").value.trim().toLowerCase();
     const cuerpo = document.querySelector("#cuerpo");
-    const valor2 = valor.value.trim().toLowerCase();
-
-    const busqueda = pelis.filter(p => p.Title.toLowerCase().includes(valor2) || p.Director.toLowerCase().includes(valor2) || p.Actors.toLowerCase().includes(valor2));
+    const paisSeleccionado = document.querySelector("#country").value;
+    const yearDesde = parseInt(document.querySelector("#year").value);
+    const yearHasta = parseInt(document.querySelector("#year2").value);
+    const generosSeleccionados = Array.from(document.querySelectorAll("#meterGeneros input:checked"))
+        .map(g => g.value);
 
     cuerpo.innerHTML = "";
+    const buscarPorTitulo = document.querySelector("#title").checked; // para que me busque por lo seleccionado
+    const buscarPorDirector = document.querySelector("#director").checked;
+    const buscarPorActores = document.querySelector("#actors").checked;
 
-    // numero busqueda
-    const parrafo = document.createElement("p")
-    parrafo.className = "m-5 fw-bold"
-    cuerpo.appendChild(parrafo);
+    const busqueda = pelis.filter(p => {
+        let cumpleTexto;
+        if (buscarPorTitulo && buscarPorDirector && buscarPorActores) {
+            cumpleTexto = valorTexto ? p.Title.toLowerCase().includes(valorTexto) || p.Director.toLowerCase().includes(valorTexto) || p.Actors.toLowerCase().includes(valorTexto) : true;
+        } else if (buscarPorDirector) {
+            cumpleTexto = p.Director.toLowerCase().includes(valorTexto)
+        } else if (buscarPorActores) {
+            cumpleTexto = p.Actors.toLowerCase().includes(valorTexto)
+        } else if (buscarPorTitulo) {
+            cumpleTexto = p.Title.toLowerCase().includes(valorTexto);
+        } else {
+            cumpleTexto = valorTexto ? p.Title.toLowerCase().includes(valorTexto) || p.Director.toLowerCase().includes(valorTexto) || p.Actors.toLowerCase().includes(valorTexto) : true;
+        }
+        const cumplePais = paisSeleccionado === "All countries" || p.Country === paisSeleccionado;
+        const cumpleYear = (!yearDesde || p.Year >= yearDesde) && (!yearHasta || p.Year <= yearHasta);
+        const cumpleGenero = generosSeleccionados.length === 0 || generosSeleccionados.some(g => p.Genre.includes(g));
+
+        return cumpleTexto && cumplePais && cumpleYear && cumpleGenero;
+    });
+
+    const parrafo = document.createElement("p");
+    parrafo.className = "m-5 fw-bold";
     parrafo.innerHTML = busqueda.length;
-
-
+    cuerpo.appendChild(parrafo);
 
     if (busqueda.length > 0) {
         busqueda.forEach(b => {
@@ -274,42 +154,69 @@ function busquedaGeneral() {
 
             generos.innerHTML = b.Genre;
 
-            boton.innerHTML = "Details"
-            boton.className = "btn bg-warning text-dark"
+            boton.innerHTML = "Details";
+            boton.className = "btn bg-warning text-dark";
 
-            div.className = "m-5"
+            div.className = "m-5";
             div.appendChild(img);
             div.appendChild(h3);
             div.appendChild(generos);
-            div.appendChild(boton)
+            div.appendChild(boton);
 
+
+            // imdb
+            const detallesPeli = document.createElement("div");
+            detallesPeli.style.display = "none";
+            detallesPeli.innerHTML = `<pre>${JSON.stringify(b, null, 2)}</pre>`;
+
+            const inputRating = document.createElement("input");
+            inputRating.type = "number";
+            inputRating.min = "0";
+            inputRating.max = "10";
+            inputRating.step = "0.1";
+            inputRating.value = b.imdbRating || 0;
+
+            const botonActualizar = document.createElement("button");
+            botonActualizar.textContent = "Update";
+            botonActualizar.addEventListener("click", () => {
+                const nuevoRating = parseFloat(inputRating.value);
+                if (nuevoRating >= 0 && nuevoRating <= 10) {
+                    b.imdbRating = nuevoRating;
+                    detallesPeli.innerHTML += `
+                        <h3>Nueva nota ${b.imdbRating}</h3>`
+                    console.log(b.imdbRating);
+
+                } else {
+                    alert("Introduce un rating válido entre 0 y 10.");
+                }
+            });
+
+            const botonCerrar = document.createElement("button");
+            botonCerrar.textContent = "Close details";
+            botonCerrar.addEventListener("click", () => {
+                detallesPeli.style.display = "none";
+                boton.disabled = false;
+                div.classList.remove("bg-secondary");
+            });
+
+            detallesPeli.appendChild(inputRating);
+            detallesPeli.appendChild(botonActualizar);
+            detallesPeli.appendChild(botonCerrar);
+            div.appendChild(detallesPeli);
+
+            boton.addEventListener("click", () => {
+                detallesPeli.style.display = "block";
+                boton.disabled = true;
+                div.classList.add("bg-secondary");
+            });
 
             cuerpo.appendChild(div);
         });
     } else {
         const vacio = document.createElement("p");
         vacio.innerHTML = "No se encontraron resultados.";
-        cuerpo.appendChild(mensaje);
+        cuerpo.appendChild(vacio);
     }
 }
 
-document.querySelector("#boton").addEventListener("click", function () {
-
-    const valorTitulo = document.querySelector("#title").checked;
-    const valorDirector = document.querySelector("#director").checked;
-    const valorActor = document.querySelector("#actors").checked;
-    const cuerpo = document.querySelector("#cuerpo");
-
-    cuerpo.innerHTML = "";
-    if (!valorTitulo && !valorDirector && !valorActor) {
-        busquedaGeneral();
-    } else {
-        if (valorTitulo) {
-            busquedaTitulo();
-        } if (valorDirector) {
-            busquedaDirector();
-        } if (valorActor) {
-            busquedaActores();
-        }
-    }
-})
+document.querySelector("#boton").addEventListener("click", busquedaGeneral);

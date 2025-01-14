17be1a3629854ccb4b05b5d6d72293020ccc7d96
DavidAlonso858 <davidmadrid858@gmail.com>
2025-01-14 14:40:32 +0100

    empezando prueba angular

--- projectALUMNOSBAD/src/empresa/Empresa.js
+++
@@ -1,8 +0,0 @@
-class Empresa {
-    constructor(id, nombre, direccion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.direccion = direccion;
-    }
-}
-export default Empresa;

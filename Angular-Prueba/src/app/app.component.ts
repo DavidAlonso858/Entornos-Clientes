b4549e41c7a206976bc1f9c32f3dd08a062815b7
DavidAlonso858 <davidmadrid858@gmail.com>
2025-01-15 13:58:47 +0100

    angular primer ejercicio, crear un componente para el nav, otro para la presentacion y luego llamarlos en el html de la app

--- Angular-Prueba/src/app/app.component.ts
+++ Angular-Prueba/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { PresentacionComponent } from "./presentacion/presentacion.component";
 
 @Component({
   selector: 'app-root', // si dice que tiene que ser standalone el componente se pone standalone = true
-  imports: [RouterOutlet],
+  imports: [RouterModule, HeaderComponent, PresentacionComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

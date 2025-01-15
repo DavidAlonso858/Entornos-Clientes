e7efcf1361a8edd664f909c52ed022dc7ea62051
DavidAlonso858 <davidmadrid858@gmail.com>
2025-01-15 10:15:59 +0100

    prueba de angular + añadir boostrap en el src/style.css

--- Angular-Prueba/src/app/app.component.ts
+++ Angular-Prueba/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // si dice que tiene que ser standalone el componente se pone standalone = true
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Angular-Prueba';
+  title = 'Angular-Prueba David';
+  prueba = 'vaya frio que hace'
 }

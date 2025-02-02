f775dba3f6d4fab754f2f78b8326ca3528f7043d
david <dalogar0801@g.educaand.es>
2025-02-02 17:27:32 +0100

    conceptos Angular

--- Angular-Casas/src/app/app.component.spec.ts
+++ Angular-Casas/src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-Casas' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Casas');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- Angular-Casas/src/app/app.component.ts
+++ Angular-Casas/src/app/app.component.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
-import {HomeComponent} from './home/home.component';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [HomeComponent],
+  // se puede separar a parte con el templateUrls: 
+  // al igual que hago con el css
   template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
-      <section class="content">
+      <section class="contenpt">
+        <h4> {{ city }}</h4>
         <app-home></app-home>
       </section>
     </main>
@@ -18,6 +21,6 @@ import {HomeComponent} from './home/home.component';
 })
 
 export class AppComponent {
-  title = 'homes';
+  city = 'Algeciras';
 }
 
